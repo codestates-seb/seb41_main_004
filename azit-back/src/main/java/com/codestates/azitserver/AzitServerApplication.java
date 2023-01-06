f6975126df1e792374a677fecfb33c26d0c77f38
my-roki <anthonychangrok@gmail.com>
2023-01-06 15:18:39 +0900

    build : backend server

---
+++ azit-back/src/main/java/com/codestates/azitserver/AzitServerApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.azitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzitServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzitServerApplication.class, args);
+	}
+
+}
---
+++ azit-back/src/test/java/com/codestates/azitserver/AzitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.azitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzitServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
