e9a0ac4ebd0f383e1dd6df09d1921998aeac2513
my-roki <anthonychangrok@gmail.com>
2023-01-09 18:56:16 +0900

    [#22] feat: BaseEntity 구현

--- azit-back/src/main/java/com/codestates/azitserver/AzitServerApplication.java
+++ azit-back/src/main/java/com/codestates/azitserver/AzitServerApplication.java
@@ -3,7 +3,9 @@ package com.codestates.azitserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @PropertySource("classpath:/env.properties")
 public class AzitServerApplication {
