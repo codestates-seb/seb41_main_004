9a2636fab0d7bf371ba23739a2f9209be690e9e2
Zoe Park <mary990t@gmail.com>
2023-01-10 14:45:10 +0900

    chore: Loading page Markup

---
+++ azit-front/src/pages/Loading.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import LogoWhite from "../images/logoWhite.png";
+
+const LoadingContainer = styled.div`
+  background-color: var(--point-color);
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > img {
+    width: 16.182rem;
+    height: 7rem;
+  }
+`
+
+const Loading = () => {
+  return (
+    <>
+      <LoadingContainer>
+        <img src={LogoWhite}></img>
+      </LoadingContainer>
+    </>
+  )
+}
+
+export default Loading;
