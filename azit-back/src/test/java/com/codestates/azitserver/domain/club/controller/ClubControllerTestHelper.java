01246a10d1637bfa782bb29f4e921bbe0b94fe8c
my-roki <anthonychangrok@gmail.com>
2023-01-10 20:23:40 +0900

    [#16] test: ControllerTestHelper 업데이트

--- azit-back/src/test/java/com/codestates/azitserver/domain/club/controller/ClubControllerTestHelper.java
+++ azit-back/src/test/java/com/codestates/azitserver/domain/club/controller/ClubControllerTestHelper.java
@@ -5,7 +5,13 @@ import com.codestates.azitserver.global.utils.ControllerTestHelper;
 public interface ClubControllerTestHelper extends ControllerTestHelper {
 	String CLUB_URL = "/api/clubs";
 
+	String CLUB_RESOURCE_URI = "/{club-id}";
+
 	default String getClubUrl() {
 		return CLUB_URL;
+	}
+
+	default String getClubUri() {
+		return CLUB_URL + CLUB_RESOURCE_URI;
 	}
 }
