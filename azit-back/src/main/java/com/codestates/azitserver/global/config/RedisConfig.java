467838ad3a6b346fde41ff5b657d210f7ed5e7fe
yeonini <leeyeon.cd@gmail.com>
2023-01-10 16:29:19 +0900

    fix: RedisConfig 수정

--- azit-back/src/main/java/com/codestates/azitserver/global/config/RedisConfig.java
+++ azit-back/src/main/java/com/codestates/azitserver/global/config/RedisConfig.java
@@ -17,8 +17,8 @@ public class RedisConfig {
 	@Value("${redis.port}")
 	private int port;
 
-	@Value("${redis.password}")
-	private String password;
+	// @Value("${redis.password}")
+	// private String password;
 
 	@Bean
 	public LettuceConnectionFactory redisConnectionFactory() {
