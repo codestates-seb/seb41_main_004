ae15fe17ebafda6eb1b1d95e8b1e957234466209
yeonini <leeyeon.cd@gmail.com>
2023-01-10 09:06:35 +0900

    feat: RedisConfig 작성

---
+++ azit-back/src/main/java/com/codestates/azitserver/global/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.codestates.azitserver.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+	// RedisTemplate를 통해 레디스와 통신
+
+	@Value("${redis.host}")
+	private String host;
+
+	@Value("${redis.port}")
+	private int port;
+
+	@Bean
+	public LettuceConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(host, port);
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+		//key, value serializer
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+		return redisTemplate;
+	}
+}
