7143590f4f0ca7497bbc7154d9bc7a1573af93fc
FE012 <twte96@gmail.com>
2023-01-11 15:24:59 +0900

    chore:App.js path 수정

--- azit-front/src/App.js
+++ azit-front/src/App.js
@@ -39,7 +39,7 @@ function App() {
           <Route path="/signupadd" element={<SignupAdditional />}></Route>
           <Route path="/userpage" element={<UserPage />}></Route>
           <Route path="/userpage/setting" element={<UserPageSetting />}></Route>
-          <Route path="/userpage/user_report" element={<UserReport />}></Route>
+          <Route path="/userpage/userreport" element={<UserReport />}></Route>
           <Route path="/azitsetting" element={<AzitSetting />}></Route>
           <Route path="/azitedit" element={<AzitEdit />}></Route>
           <Route path="/azitjoin" element={<AzitJoin />}></Route>
