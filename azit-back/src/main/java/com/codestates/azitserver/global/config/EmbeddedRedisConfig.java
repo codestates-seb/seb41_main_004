df7633b1ffea2abbb413f6f0f40b2af59a363323
yeonini <leeyeon.cd@gmail.com>
2023-01-11 23:12:28 +0900

    fix: 내장 Redis 에 예외처리 추가

--- azit-back/src/main/java/com/codestates/azitserver/global/config/EmbeddedRedisConfig.java
+++ azit-back/src/main/java/com/codestates/azitserver/global/config/EmbeddedRedisConfig.java
@@ -33,7 +33,14 @@ public class EmbeddedRedisConfig {
 			.port(port)
 			.setting("maxmemory 128M")
 			.build();
-		redisServer.start();
+
+		try {
+			redisServer.start();
+		} catch (Exception e) {
+			log.error("이미 실행중인 포트", e);
+		}
+
+		// System.out.println("내장 REDIS 진행 중");
 	}
 
 	@PreDestroy // 빈 제거하기 전에 마지막으로 해야하는 작업에 사용하는 어노테이션
@@ -41,53 +48,53 @@ public class EmbeddedRedisConfig {
 		Optional.ofNullable(redisServer).ifPresent(RedisServer::stop);
 	}
 
-	/**
-	 * Embedded Redis가 현재 실행중인지 확인
-	 */
-	private boolean isRedisRunning() throws IOException {
-		return isRunning(executeGrepProcessCommand(port));
-	}
-
-	/**
-	 * 현재 PC/서버에서 사용가능한 포트 조회
-	 */
-	public int findAvailablePort() throws IOException {
-
-		for (int port = 10000; port <= 65535; port++) {
-			Process process = executeGrepProcessCommand(port);
-			if (!isRunning(process)) {
-				return port;
-			}
-		}
-
-		throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
-	}
-
-	/**
-	 * 해당 port를 사용중인 프로세스 확인하는 sh 실행
-	 */
-	private Process executeGrepProcessCommand(int port) throws IOException {
-		String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
-		String[] shell = {"/bin/sh", "-c", command};
-		return Runtime.getRuntime().exec(shell);
-	}
-
-	/**
-	 * 해당 Process가 현재 실행중인지 확인
-	 */
-	private boolean isRunning(Process process) {
-		String line;
-		StringBuilder pidInfo = new StringBuilder();
-
-		try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-
-			while ((line = input.readLine()) != null) {
-				pidInfo.append(line);
-			}
-
-		} catch (Exception e) {
-		}
-
-		return StringUtils.hasText(pidInfo.toString());
-	}
+	// /**
+	//  * Embedded Redis가 현재 실행중인지 확인
+	//  */
+	// private boolean isRedisRunning() throws IOException {
+	// 	return isRunning(executeGrepProcessCommand(port));
+	// }
+	//
+	// /**
+	//  * 현재 PC/서버에서 사용가능한 포트 조회
+	//  */
+	// public int findAvailablePort() throws IOException {
+	//
+	// 	for (int port = 10000; port <= 65535; port++) {
+	// 		Process process = executeGrepProcessCommand(port);
+	// 		if (!isRunning(process)) {
+	// 			return port;
+	// 		}
+	// 	}
+	//
+	// 	throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+	// }
+	//
+	// /**
+	//  * 해당 port를 사용중인 프로세스 확인하는 sh 실행
+	//  */
+	// private Process executeGrepProcessCommand(int port) throws IOException {
+	// 	String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
+	// 	String[] shell = {"/bin/sh", "-c", command};
+	// 	return Runtime.getRuntime().exec(shell);
+	// }
+	//
+	// /**
+	//  * 해당 Process가 현재 실행중인지 확인
+	//  */
+	// private boolean isRunning(Process process) {
+	// 	String line;
+	// 	StringBuilder pidInfo = new StringBuilder();
+	//
+	// 	try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+	//
+	// 		while ((line = input.readLine()) != null) {
+	// 			pidInfo.append(line);
+	// 		}
+	//
+	// 	} catch (Exception e) {
+	// 	}
+	//
+	// 	return StringUtils.hasText(pidInfo.toString());
+	// }
 }
