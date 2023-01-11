ec686aa83ae6a8169b145d2a8300cc5de14c5d33
yeonini <leeyeon.cd@gmail.com>
2023-01-11 23:59:19 +0900

    Merge branch

--- azit-back/src/main/java/com/codestates/azitserver/global/config/EmbeddedRedisConfig.java
+++ azit-back/src/main/java/com/codestates/azitserver/global/config/EmbeddedRedisConfig.java
@@ -1,8 +1,6 @@
 package com.codestates.azitserver.global.config;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Optional;
 
 import javax.annotation.PostConstruct;
@@ -11,7 +9,6 @@ import javax.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.util.StringUtils;
 
 import lombok.extern.slf4j.Slf4j;
 import redis.embedded.RedisServer;
--- azit-back/src/main/java/com/codestates/azitserver/global/config/RedisConfig.java
+++ azit-back/src/main/java/com/codestates/azitserver/global/config/RedisConfig.java
@@ -1,11 +1,8 @@
 package com.codestates.azitserver.global.config;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
