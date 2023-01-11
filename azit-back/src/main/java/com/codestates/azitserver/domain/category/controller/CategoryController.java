54b1353eec86d195cfeadcc7c3e6e755e20de606
my-roki <anthonychangrok@gmail.com>
2023-01-11 22:02:58 +0900

    [#80] feat: 카테고리 controller 및 dto 작성

---
+++ azit-back/src/main/java/com/codestates/azitserver/domain/category/controller/CategoryController.java
@@ -0,0 +1,53 @@
+package com.codestates.azitserver.domain.category.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codestates.azitserver.domain.category.dto.CategoryDto;
+import com.codestates.azitserver.domain.category.entity.CategoryLarge;
+import com.codestates.azitserver.domain.category.entity.CategorySmall;
+import com.codestates.azitserver.domain.category.mapper.CategoryMapper;
+import com.codestates.azitserver.domain.category.service.CategoryService;
+import com.codestates.azitserver.global.dto.SingleResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/categories")
+public class CategoryController {
+	private final CategoryService categoryService;
+	private final CategoryMapper mapper;
+
+
+	@GetMapping
+	public ResponseEntity<?> getAllCategory() {
+		List<CategoryLarge> categoryLargeList = categoryService.getAllLargeCategory();
+		List<CategoryDto.Response> responses = mapper.categoryLargeToCategoryResponseDto(categoryLargeList);
+
+		return new ResponseEntity<>(new SingleResponseDto<>(responses), HttpStatus.OK);
+	}
+
+	@GetMapping("/large")
+	public ResponseEntity<?> getLargeCategory() {
+		List<CategoryLarge> categoryLargeList = categoryService.getAllLargeCategory();
+		List<CategoryDto.LargeResponse> responses = mapper.categoryLargeToCategoryLargeResponseDto(categoryLargeList);
+
+		return new ResponseEntity<>(new SingleResponseDto<>(responses), HttpStatus.OK);
+	}
+
+	@GetMapping("/small")
+	public ResponseEntity<?> getSmallCategory() {
+		List<CategorySmall> categorySmallList = categoryService.getAllSmallCategory();
+		List<CategoryDto.SmallResponse> responses = mapper.categoryLargeToCategorySmallResponseDto(categorySmallList);
+
+		return new ResponseEntity<>(new SingleResponseDto<>(responses), HttpStatus.OK);
+	}
+}
