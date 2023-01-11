846e6280dd187ef6e10ed41623079f2671491011
CHOGANGYEOL <loeygnak@gmail.com>
2023-01-11 10:16:31 +0900

    chore:button css 수정

--- azit-front/src/components/Button.js
+++ azit-front/src/components/Button.js
@@ -1,18 +1,31 @@
 import styled from "styled-components";
 
 const ButtonWrap = styled.button`
+width: 100%;
   height: 55px;
-  background-color: var(--point-color);
   font-size: var(--big-font);
-  color: var(--white-color);
   border-radius: 5px;
   border: none;
-  margin-right: 2rem;
-  margin-left: 2rem;
+  margin:0;
+  padding: 0;
+  cursor: pointer;
+  transition:0.5s all;
+  &.active {
+    background-color: var(--point-color);
+    color: var(--white-color);
+  }
+  &.disabled {
+    background-color: var(--border-color);
+    color: var(--light-font-color);
+    pointer-events: none;
+  }
+  :hover {
+    background-color: var(--hover-color);
+  }
 `;
 
-const Button = ({ title }) => {
-  return <ButtonWrap>{title}</ButtonWrap>;
+const Button = ({ title, state }) => {
+  return <ButtonWrap className={state}>{title}</ButtonWrap>;
 };
 
 export default Button;
