0a3774fac7b15660af9f7fcabcff00c5b04f1737
my-roki <anthonychangrok@gmail.com>
2023-01-16 16:35:52 +0900

    refact: 아지트 생성시 날짜와 시간 값 분리로 날짜별 조회 로직 수정

--- azit-back/src/main/java/com/codestates/azitserver/domain/club/repository/ClubRepository.java
+++ azit-back/src/main/java/com/codestates/azitserver/domain/club/repository/ClubRepository.java
@@ -1,6 +1,6 @@
 package com.codestates.azitserver.domain.club.repository;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,10 +15,9 @@ public interface ClubRepository extends JpaRepository<Club, Long> {
 	@Query("select c from Club c where c.categorySmall.categoryLarge.CategoryLargeId = :categoryLargeId")
 	Page<Club> findAllClubByCategoryLargeId(Long categoryLargeId, Pageable pageable);
 
-	// SELECT * FROM CLUB WHERE MEETING_DATE BETWEEN {시작} AND {끝}
-	@Query("select c from Club c where c.meetingDate between :targetDateBefore and :targetDate")
-	Page<Club> findAllClubsByClubMeetingDate(LocalDateTime targetDateBefore, LocalDateTime targetDate,
-		PageRequest createdAt);
+	// SELECT * FROM CLUB WHERE MEETING_DATE = {날짜}
+	@Query("select c from Club c where c.meetingDate = :targetDate")
+	Page<Club> findAllClubsByClubMeetingDate(LocalDate targetDate, PageRequest createdAt);
 
 	// SELECT * FROM CLUB WHERE CLUB_NAME LIKE '%{keyword}%' OR CLUB_INFO LIKE '%{keyword}%';
 	@Query("select c from Club c where c.clubName like concat('%', :keyword, '%') or c.clubInfo like concat('%', :keyword, '%')")
