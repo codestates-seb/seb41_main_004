65f37b1d53600bad143e72df789b6aaab31161ed
CHOGANGYEOL <loeygnak@gmail.com>
2023-01-18 13:55:35 +0900

    feat:카테고리 별 아지트 조회 기능 구현 완료

--- azit-front/src/components/Home/CategoryTab.js
+++ azit-front/src/components/Home/CategoryTab.js
@@ -1,61 +1,77 @@
 import styled from "styled-components";
 import { ClubData } from "../../dummyData/ClubData";
 import AzitList from "../common/AzitList";
-import { useState } from "react";
-import { useQuery } from "react-query";
+import React, { useEffect, useState } from "react";
+import { useInfiniteQuery } from "react-query";
 import { axiosInstance } from "../../util/axios";
 import CategoryPicker from "./CategoryPicker";
+import { useInView } from "react-intersection-observer";
+import Loading from "../common/Loading";
 
 const Null = styled.article`
   padding: 8rem 0 0;
   text-align: center;
   color: var(--sub-font-color);
 `;
-const MoreBtn = styled.button`
-  width: 100%;
-  height: 4rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  font-size: var(--big-font);
-  font-weight: var(--bold-weight);
-  color: var(--sub-font-color);
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-`;
 const CategoryTab = () => {
   const [selectCategory, setSelectCategory] = useState(1);
+  const { ref, inView } = useInView();
   const handleSelectCategory = (num) => {
     setSelectCategory(num);
   };
-  // Data를 카테고리별로 data를 받아오는 로직 필요
-  const getSelectCategory = async (cl) => {
-    try {
-      const res = await axiosInstance.get(
-        `/api/clubs/category?page=1&size=10&cl=${cl}`
-      );
 
-      return res.data;
-    } catch (error) {
-      console.log(error.response.data);
-    }
+  const fetchInfiniteList = async (pageParam, cl) => {
+    const res = await axiosInstance.get(
+      `/api/clubs/category?page=${pageParam}&size=5&cl=${cl}`
+    );
+    return {
+      board_page: res.data.data,
+      nextPage: pageParam + 1,
+      isLast:
+        res.data.data.length > 0
+          ? res.data.pageInfo.page === res.data.pageInfo.totalPages
+          : true,
+    };
   };
-  const { data, isLoading, isError, error } = useQuery(
-    ["selectData", selectCategory],
-    () => getSelectCategory(selectCategory)
+
+  const { data, status, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
+    ["get", selectCategory],
+    ({ pageParam = 1 }) => fetchInfiniteList(pageParam, selectCategory),
+    {
+      enabled: !!selectCategory,
+      staleTime: 6 * 10 * 1000,
+      cacheTime: 6 * 10 * 1000,
+      getNextPageParam: (lastPage) =>
+        !lastPage.isLast ? lastPage.nextPage : undefined,
+    }
   );
-  console.log(data)
+
+  useEffect(() => {
+    if (inView) fetchNextPage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
   return (
     <>
-    <CategoryPicker selectCategory={selectCategory} handleSelectCategory={handleSelectCategory}/>
-      {data?.data ? (
-        data.data.map((data) => <AzitList key={data.clubId} data={data} />)
-      ) : (
-        <Null>해당 카테고리의 아지트가 없습니다.</Null>
-      )}
-      {ClubData && <MoreBtn>더보기 +</MoreBtn>}
+      <CategoryPicker
+        selectCategory={selectCategory}
+        handleSelectCategory={handleSelectCategory}
+      />
+      <article className="resultCell">
+        {status === "loading" && <Loading />}
+        {status === "error" && <Null>Not Defined Error</Null>}
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.board_page.length > 0 ? (
+              page.board_page.map((searchData) => (
+                <AzitList key={searchData.clubId} data={searchData} />
+              ))
+            ) : (
+              <Null>해당 카테고리의 아지트가 없습니다.</Null>
+            )}
+          </React.Fragment>
+        ))}
+      </article>
+      {isFetchingNextPage ? <Loading /> : <div ref={ref} />}
     </>
   );
 };
