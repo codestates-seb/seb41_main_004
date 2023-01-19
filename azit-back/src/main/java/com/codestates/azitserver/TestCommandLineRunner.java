9cb4120b313babf0bcf7d01b9016783e91e4ff8f
my-roki <anthonychangrok@gmail.com>
2023-01-19 09:35:45 +0900

    deploy : 파라미터 스토어 등록을 위한 작업

---
+++ azit-back/src/main/java/com/codestates/azitserver/TestCommandLineRunner.java
@@ -0,0 +1,20 @@
+package com.codestates.azitserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class TestCommandLineRunner implements CommandLineRunner {
+	@Value("${ADMIN_EMAIL}")
+	private String email;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("-".repeat(88));
+		System.out.println(email);
+	}
+}
