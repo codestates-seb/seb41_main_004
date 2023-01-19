f9b7f790a6a963dce482ef94b119d131a30c991b
cryoon <anthonychangrok@gmail.com>
2023-01-19 17:01:54 +0900

    [hotfix] 예외처리, 아지트 배너 이미지 경로 설정 문제 (#217)
    * fix : BusinessLogicException 응답 상태값을 정상적으로 반환하지 않는 문제 수정
    
    * fix : 파일 저장 경로 문제 수정
    
    * fix : 테스트를 위한 로그 출력

--- azit-back/src/main/java/com/codestates/azitserver/global/exception/StorageException.java
+++ azit-back/src/main/java/com/codestates/azitserver/global/exception/StorageException.java
@@ -1,5 +1,6 @@
 package com.codestates.azitserver.global.exception;
 
+// TODO : Storage excrption은 현재 500번대 에러를 출력합니다.
 public class StorageException extends RuntimeException {
 	public StorageException(String message) {
 		super(message);
