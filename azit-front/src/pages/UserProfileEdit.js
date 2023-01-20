f6c84d7b58be7ec653b944ced0ddec839709504d
FE012 <twte96@gmail.com>
2023-01-20 15:02:08 +0900

    fix:유저페이지 수정

--- azit-front/src/pages/UserProfileEdit.js
+++ azit-front/src/pages/UserProfileEdit.js
@@ -221,6 +221,7 @@ const UserProfileEdit = () => {
         }
       })
       .catch((error) => {
+        alert("닉네임이 중복되었습니다. 다시 시도하세요."); // alert 열리게 고침
         console.log(error);
       });
   };
