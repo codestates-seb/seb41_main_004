195811f4688d0fdce4c6b14a44d550663df20412
my-roki <anthonychangrok@gmail.com>
2023-01-20 11:26:25 +0900

    로그 파일 권한 수정

--- azit-back/scripts/deploy.sh
+++ azit-back/scripts/deploy.sh
@@ -27,6 +27,7 @@ fi
 UUID=$(uuidgen)
 echo "> 로그파일 생성 $UUID.log" >> $BUILD_LOG
 sudo mkdir -p /home/ubuntu/action/logs && sudo touch /home/ubuntu/action/logs/$UUID.log
+sudo chmod -R 777 /home/ubuntu/action/logs/$UUID.log
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> $BUILD_LOG
