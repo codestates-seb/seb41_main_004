f35bfa80868834ef1f9818c0c0a2d25511f19ae6
Zoe Park <mary990t@gmail.com>
2023-01-26 11:12:55 +0900

    fix: 로그아웃 통신 axios로 변경

--- azit-front/src/util/axios.js
+++ azit-front/src/util/axios.js
@@ -2,51 +2,12 @@ import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { getCookie } from "./cookie/cookie";
 
+// axios.defaults.withCredentials = true;
 
 export const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL
 })
 
-axiosInstance.interceptors.request.use(async (req) => {
-  const accessToken = localStorage.getItem("accessToken");
-  const refreshToken = getCookie("refreshToken");
-  const email = localStorage.getItem("email");
 
-  const expired = Date.now() >= jwtDecode(accessToken).exp * 1000;
-  // console.log(accessToken);
-  // console.log(refreshToken);
-  // console.log(expired);
-
-  if (accessToken && !expired) {
-    return req;
-  } else {
-    console.log("intercept");
-
-    try {
-      const body = {
-        email: email,
-        accessToken: accessToken,
-      };
-      const res = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}api/auth/reIssue`,
-        body,
-        {
-          headers: {
-            Refresh: refreshToken,
-          },
-          "Content-Type": "application/json"
-        }
-      );
-      console.log(res);
-
-      localStorage.setItem("accessToken", res.headers.get("Authorization"));
-    } catch (e) {
-      console.log(e.response.data);
-      console.log("에러");
-    }
-
-    return req;
-  }
-});
 
 
