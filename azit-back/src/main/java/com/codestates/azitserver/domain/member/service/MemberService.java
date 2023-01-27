d7d0dae6bffa890a0c4e452c05ded074807e10c4
hionpu <111286364+hionpu@users.noreply.github.com>
2023-01-27 11:35:51 +0900

    [member][club] 활동내역 조회에 아지트 정보, 참여자 정보 포함 (#362)

--- azit-back/src/main/java/com/codestates/azitserver/domain/member/service/MemberService.java
+++ azit-back/src/main/java/com/codestates/azitserver/domain/member/service/MemberService.java
@@ -17,9 +17,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.codestates.azitserver.domain.category.entity.CategorySmall;
 import com.codestates.azitserver.domain.category.service.CategoryService;
+import com.codestates.azitserver.domain.club.dto.ClubDto;
 import com.codestates.azitserver.domain.club.dto.ClubMemberDto;
 import com.codestates.azitserver.domain.club.entity.Club;
 import com.codestates.azitserver.domain.club.entity.ClubMember;
+import com.codestates.azitserver.domain.club.mapper.ClubMapper;
 import com.codestates.azitserver.domain.club.mapper.ClubMemberMapper;
 import com.codestates.azitserver.domain.club.repository.ClubMemberRepository;
 import com.codestates.azitserver.domain.club.repository.ClubRepository;
@@ -30,6 +32,7 @@ import com.codestates.azitserver.domain.fileInfo.service.StorageService;
 import com.codestates.azitserver.domain.member.dto.MemberDto;
 import com.codestates.azitserver.domain.member.entity.Member;
 import com.codestates.azitserver.domain.member.entity.MemberCategory;
+import com.codestates.azitserver.domain.member.mapper.MemberMapper;
 import com.codestates.azitserver.domain.member.repository.MemberCategoryRepository;
 import com.codestates.azitserver.domain.member.repository.MemberRepository;
 import com.codestates.azitserver.domain.review.entity.Review;
@@ -54,6 +57,8 @@ public class MemberService {
 	private final ClubMemberMapper clubMemberMapper;
 	private final ReviewRepository reviewRepository;
 
+	private final ClubMapper clubMapper;
+	private final MemberMapper memberMapper;
 	//회원 생성
 	public Member createMember(Member tempMember, MultipartFile profileImage, List<Long> categorySmallIdList) {
 		// 닉네임 중복 확인
@@ -289,15 +294,27 @@ public class MemberService {
 		for (ClubMember clubMember : clubMemberList) {
 			ClubMemberDto.ClubMemberStatusResponse response =
 				clubMemberMapper.clubMemberToClubMemberDtoClubMemberStatusResponse(clubMember);
-			// Club Id
-			response.setClubId(clubMember.getClub().getClubId());
+			Club club = clubMember.getClub();
+			// Club Info
+			ClubDto.ClubMemberStatusClubInfoResponse clubInfoResponse
+				= clubMapper.clubToClubMemberStatusClubInfoResponse(club);
+				// Club Info 에 참여자들 정보 넣기
+			List<ClubMember> preParticipants = club.getClubMembers();
+			List<MemberDto.ParticipantResponse> participantResponses= new ArrayList<>();
+			for (ClubMember pre : preParticipants) {
+				participantResponses.add(memberMapper.memberToParticipantResponse(pre.getMember()));
+			}
+			clubInfoResponse.setParticipantList(participantResponses);
+				// 참여자 정보넣기 끗
+
+			response.setClubInfoResponse(clubInfoResponse);
+
 			// Host 여부
 			response.setIsHost(clubMember.getClub().getHost().getMemberId()==clubMember.getMember().getMemberId());
 			// 숨김상태인지
 			response.setIsHidden(false);
 			// 리뷰 작성 했는지
 			Member member = clubMember.getMember();
-			Club club = clubMember.getClub();
 			List<Review> reviewList = reviewRepository.findAllReviewsByReviewerAndClub(member, club);
 			if ( reviewList != null && !reviewList.isEmpty()) {
 				response.setIsReviewed(true);
--- azit-back/src/test/java/com/codestates/azitserver/domain/stub/ClubMemberStubData.java
+++ azit-back/src/test/java/com/codestates/azitserver/domain/stub/ClubMemberStubData.java
@@ -1,5 +1,9 @@
 package com.codestates.azitserver.domain.stub;
 
+import static com.codestates.azitserver.domain.stub.CategoryStubData.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
@@ -7,8 +11,11 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import com.codestates.azitserver.domain.club.dto.ClubDto;
 import com.codestates.azitserver.domain.club.dto.ClubMemberDto;
+import com.codestates.azitserver.domain.club.entity.Club;
 import com.codestates.azitserver.domain.club.entity.ClubMember;
+import com.codestates.azitserver.domain.member.dto.MemberDto;
 
 public class ClubMemberStubData {
 	public static ClubMember getDefaultClubMember() {
@@ -57,4 +64,47 @@ public class ClubMemberStubData {
 			Sort.by("createdAt").descending()), 1);
 	}
 
+	public static ClubMemberDto.ClubMemberStatusResponse getClubMemberStatusDtoResponse(Long index) {
+		ClubMemberDto.ClubMemberStatusResponse response = new ClubMemberDto.ClubMemberStatusResponse();
+
+		response.setClubMemberId(index);
+		response.setClubInfoResponse(getClubMemberStatusClubInfoResponse());
+		response.setClubMemberStatus(ClubMember.ClubMemberStatus.CLUB_WAITING);
+		response.setIsHost(false);
+		response.setIsHidden(false);
+		response.setIsReviewed(false);
+
+		return response;
+	}
+
+	public static ClubDto.ClubMemberStatusClubInfoResponse getClubMemberStatusClubInfoResponse() {
+		ClubDto.ClubMemberStatusClubInfoResponse response = new ClubDto.ClubMemberStatusClubInfoResponse();
+
+		response.setClubId(1L);
+		response.setClubName("재밌는 아지트");
+		response.setClubInfo("재밌는 아지트입니다.");
+		response.setMemberLimit(4);
+		response.setMeetingDate(LocalDate.now().plusDays(1));
+		response.setMeetingTime(LocalTime.now().plusHours(1));
+		response.setIsOnline("offline");
+		response.setLocation("서울시 송파구");
+		response.setClubStatus(Club.ClubStatus.CLUB_ACTIVE);
+		response.setHost(MemberStubData.stubMemberIdAndNickname());
+		response.setCategorySmall(getSmallResponse());
+		response.setBannerImage(FileInfoStubData.getFileInfoDtoResponse());
+		response.setParticipantList(List.of(getParticipantResponse(1L), getParticipantResponse(2L),
+			getParticipantResponse(3L)));
+
+		return response;
+	}
+
+	public static MemberDto.ParticipantResponse getParticipantResponse(Long index) {
+		MemberDto.ParticipantResponse response = new MemberDto.ParticipantResponse();
+
+		response.setMemberId(index);
+		response.setNickname("참여자"+index);
+		response.setFileInfo(FileInfoStubData.getFileInfoDtoResponse());
+
+		return response;
+	}
 }
